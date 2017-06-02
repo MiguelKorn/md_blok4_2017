c5f4b5540165ba7583788a469a12c629a455a895
Miguel Korn <20448@ma-web.nl>
2017-06-02 13:26:54 +0200

    les 1

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ import Notification from './notification';
 
 
 const notificationBar = new Notification(".notification-bar");
+const notificationBarTwo = new Notification(".notification-barTwo");
 let countClick = 0;
 
 notificationBar.showMessage("Welkom op deze pagina");
+notificationBarTwo.showMessage("Welkom op deze pagina2");
 
 document.addEventListener("click",
                           () => {
                               countClick++;
                               notificationBar.showMessage(`je hebt ${countClick}x geklikt`);
+                              notificationBarTwo.showMessage(`je hebt ${countClick}x geklikt!`);
                           });
